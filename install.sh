a6659e2500bcfeab30e11ea9f2f2de34a75dd6c1
Daniel Mabbett <15195905+DanielMabbett@users.noreply.github.com>
2021-11-15 15:52:17 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ VERSION="v0.2.2"
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   wget "https://github.com/DanielMabbett/terrabot/releases/download/${VERSION}/terrabot-${VERSION}-linux-amd64.tar.gz"
   tar -xzvf terrabot-${VERSION}-linux-amd64.tar.gz
+  rm terrabot-${VERSION}-linux-amd64.tar.gz
 else
   echo "[Information] Operating System $OSTYPE not supported."
 fi
